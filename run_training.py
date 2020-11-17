932245e5f7c15d37e9805fd3a37e36dbd25b57bb
mahdinejad <62427625+mahdinejad@users.noreply.github.com>
2020-11-17 00:08:29 +0000

    Python2 to 3

--- run_training.py
+++ run_training.py
@@ -5,41 +5,43 @@
 ##################################################
 
 import os, sys
-import ConfigParser
+import configparser
 
+# config file to read from
+import parser
 
-#config file to read from
-config = ConfigParser.RawConfigParser()
-config.readfp(open(r'./configuration.txt'))
-#===========================================
-#name of the experiment
+config = configparser.RawConfigParser()
+config.read_file(open(r'./configuration.txt'))
+# ===========================================
+# name of the experiment
 name_experiment = config.get('experiment name', 'name')
-nohup = config.getboolean('training settings', 'nohup')   #std output on log file?
+nohup = config.getboolean('training settings', 'nohup')  # std output on log file?
 
 run_GPU = '' if sys.platform == 'win32' else ' THEANO_FLAGS=device=gpu,floatX=float32 '
 
-#create a folder for the results
+# create a folder for the results
 result_dir = name_experiment
-print "\n1. Create directory for the results (if not already existing)"
+print("\n1. Create directory for the results (if not already existing)")
 if os.path.exists(result_dir):
-    print "Dir already existing"
-elif sys.platform=='win32':
+    print("Dir already existing")
+elif sys.platform == 'win32':
     os.system('mkdir ' + result_dir)
 else:
-    os.system('mkdir -p ' +result_dir)
+    os.system('mkdir -p ' + result_dir)
 
-print "copy the configuration file in the results folder"
-if sys.platform=='win32':
-    os.system('copy configuration.txt .\\' +name_experiment+'\\'+name_experiment+'_configuration.txt')
+print("copy the configuration file in the results folder")
+if sys.platform == 'win32':
+    os.system('copy configuration.txt .\\' + name_experiment + '\\' + name_experiment + '_configuration.txt')
 else:
-    os.system('cp configuration.txt ./' +name_experiment+'/'+name_experiment+'_configuration.txt')
+    os.system('cp configuration.txt ./' + name_experiment + '/' + name_experiment + '_configuration.txt')
 
 # run the experiment
 if nohup:
-    print "\n2. Run the training on GPU with nohup"
-    os.system(run_GPU +' nohup python -u ./src/retinaNN_training.py > ' +'./'+name_experiment+'/'+name_experiment+'_training.nohup')
+    print("\n2. Run the training on GPU with nohup")
+    os.system(
+        run_GPU + ' nohup python -u ./src/retinaNN_training.py > ' + './' + name_experiment + '/' + name_experiment + '_training.nohup')
 else:
-    print "\n2. Run the training on GPU (no nohup)"
-    os.system(run_GPU +' python ./src/retinaNN_training.py')
+    print("\n2. Run the training on GPU (no nohup)")
+    os.system(run_GPU + ' python ./src/retinaNN_training.py')
 
-#Prediction/testing is run with a different script
+# Prediction/testing is run with a different script
